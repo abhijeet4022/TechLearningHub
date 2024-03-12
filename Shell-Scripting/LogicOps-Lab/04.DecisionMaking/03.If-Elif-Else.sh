86ba3d0d6b22cf2b474c43a8b3a98a760f880bb3
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-12 20:16:05 +0530

    Merge remote-tracking branch 'origin/main'

--- Shell-Scripting/LogicOps-Lab/04.DecisionMaking/03.If-Elif-Else.sh
+++ Shell-Scripting/LogicOps-Lab/04.DecisionMaking/03.If-Elif-Else.sh
@@ -3,7 +3,11 @@ number=4
 if [ $number -eq 1 ]; then
     echo "value of number is 1"
 elif [ $number -eq 4 ]; then
-    echo "value of number is 2"
+    echo "value of number is 4"
 else
     echo "Invalid value"
 fi
+
+
+
+
--- Shell-Scripting/LogicOps-Lab/04.DecisionMaking/04.If-Then-Elif-Else.sh
+++ Shell-Scripting/LogicOps-Lab/04.DecisionMaking/04.If-Then-Elif-Else.sh
@@ -2,12 +2,14 @@
 
 total=10
 
-if [ $total -eq 1000 ]
-then
- echo "total is equal to 1000"
-elif [ $total -lt 1000 ]
-then
- echo "Total is less than 1000"
+if [ $total -eq 1000 ]; then
+      echo "total is equal to 1000"
+elif [ $total -lt 1000 ]; then
+      echo "Total is less than 1000"
 else
- echo "Total is greater than 1000"
+      echo "Total is greater than 1000"
 fi
+
+
+
+
