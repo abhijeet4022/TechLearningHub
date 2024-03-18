254789152b9da85d5cf216df674e02df6c2f9fb9
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-18 19:57:32 +0530

    Package_Installation

--- Shell-Scripting/My-Script/package_installation.sh
+++ Shell-Scripting/My-Script/package_installation.sh
@@ -13,12 +13,13 @@ for package in "${PACKAGE_NAME[@]}"; do
     echo -e "\n\e[33mInstalling the $package package.\e[0m"
     yum install -y $package &>> /dev/null
     if [ $? -eq 0 ]; then
-        echo -e "\e[32m--$package Package installed successfully\e[0m"
+        echo -e "\e[32m-- $package Package installed successfully\e[0m"
     else
-        echo -e "\e[31m--$package Package installation is failed\e[0m"
+        echo -e "\e[31m-- $package Package installation is failed\e[0m"
     fi
   else
     echo -e "\n\e[32m$package Package already available in OS.\n\e[0m"
   fi
+  echo ""
 done
 
