685a7f1b11ed8d35551e42f9d119f94a39b2ddaf
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-28 20:16:10 +0530

    ShellScripting-RoboShop

--- DevOps/ECommerce-Project/ShellScripting-RoboShop/frontend.sh
+++ DevOps/ECommerce-Project/ShellScripting-RoboShop/frontend.sh
@@ -3,6 +3,6 @@ yum install nginx -y
 cp roboshop.conf /etc/nginx/default.d/
 rm -rf /usr/share/nginx/html/*
 curl -L -s -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-unzip -o /tmp/frontend.zip -d /usr/share/nginx/html/*
+unzip -o /tmp/frontend.zip -d /usr/share/nginx/html/
 systemctl enable nginx
 systemctl restart nginx
