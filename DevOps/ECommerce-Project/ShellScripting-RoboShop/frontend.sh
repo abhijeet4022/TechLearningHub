d196498af530481f09ad7bd4c29ceab7f309e62f
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-28 20:18:06 +0530

    ShellScripting-RoboShop

--- DevOps/ECommerce-Project/ShellScripting-RoboShop/frontend.sh
+++ DevOps/ECommerce-Project/ShellScripting-RoboShop/frontend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-yum install nginx -y
 cp roboshop.conf /etc/nginx/default.d/
+yum install nginx -y
+
 rm -rf /usr/share/nginx/html/*
 curl -L -s -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip -o /tmp/frontend.zip -d /usr/share/nginx/html/
