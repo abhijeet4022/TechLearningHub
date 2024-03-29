1ca046eeeaf7ab32ac0e1ef75009b669f2f4798b
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-29 15:59:25 +0530

    ShellScripting-RoboShop

---
+++ DevOps/ECommerce-Project/ShellScripting-RoboShop/user.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cp user.service /etc/systemd/system/
+cp mongodb.repo /etc/yum.repos.d/
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs mongodb-org-shell -y
+mkdir /app
+curl -L -s -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+unzip -o /tmp/user.zip -d /app
+npm install -C /app
+
+useradd roboshop
+systemctl daemon-reload
+systemctl start user.service
+systemctl enable user.service
+
+mongo --host 127.0.0.1 </app/schema/catalogue.js
+
+systemctl restart user.service
+systemctl restart nginx
