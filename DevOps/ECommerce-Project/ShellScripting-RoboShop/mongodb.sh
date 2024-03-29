edaca30f81d1133660e3869cdca1a71d459a0c4b
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-29 15:26:42 +0530

    ShellScripting-RoboShop

--- DevOps/ECommerce-Project/ShellScripting-RoboShop/mongodb.sh
+++ DevOps/ECommerce-Project/ShellScripting-RoboShop/mongodb.sh
@@ -1,7 +1,7 @@
 cp mongodb.repo /etc/yum.repos.d/
 yum install mongodb-org -y
 systemctl start mongod
-sed -i.backup '27 s/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongodb.conf
+sed -i.backup '27 s/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
 
