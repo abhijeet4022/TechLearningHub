62bca257ecd913cce7f130227c809ec45bf71ff6
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-31 14:24:26 +0530

    ShellScripting-RoboShop

--- DevOps/ECommerce-Project/ShellScripting-RoboShop/config.sh
+++ DevOps/ECommerce-Project/ShellScripting-RoboShop/config.sh
@@ -4,9 +4,9 @@ if [ -z "${component}" ]; then
   echo "Please pass the component name as argument"
   exit 1
 fi
-sudo set-hostname ${component}   &>> /dev/null
-sudo yum install git bash-completion -y &>> /dev/null
-git clone https://github.com/abhijeet4022/TechLearningHub.git &>> /dev/null
+set-hostname ${component}
+yum install git bash-completion -y
+git clone https://github.com/abhijeet4022/TechLearningHub.git
 cd TechLearningHub/DevOps/ECommerce-Project/ShellScripting-RoboShop/
 git pull &>> /dev/null ; sudo bash ${component}.sh
 
