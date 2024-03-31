1e72f0287d71cae416a055bd496ab2010ccefae3
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-31 13:28:07 +0530

    ShellScripting-RoboShop

--- DevOps/ECommerce-Project/ShellScripting-RoboShop/rabbitmq.sh
+++ DevOps/ECommerce-Project/ShellScripting-RoboShop/rabbitmq.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 source common.sh
 
-echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+echo -e "\n\e[33mConfiguring the erlang and rabbitmq repo.\e[0m" | tee -a $log
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $log
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> $log
 func_exit_status
 
 
-echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+echo -e "\n\e[33mInstalling the RabbitMQ Package.\e[0m" | tee -a $log
 yum install rabbitmq-server -y &>> $log
 func_exit_status
 
-echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+echo -e "\n\e[33mRestarting the rabbitmq-server.\e[0m" | tee -a $log
 systemctl enable rabbitmq-server &>> $log
 systemctl restart rabbitmq-server &>> $log
 func_exit_status
 
-echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+echo -e "\n\e[33mAdding user and password.\e[0m" | tee -a $log
 rabbitmqctl add_user roboshop roboshop123 &>> $log
 func_exit_status
 
-echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+echo -e "\n\e[33mSetting the permissions.\e[0m" | tee -a $log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $log
 func_exit_status
 
