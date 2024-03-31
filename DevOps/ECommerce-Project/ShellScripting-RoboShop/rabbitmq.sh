80cfdf10ef06979f79add87c48519ccb3bcdb5f8
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-31 13:25:45 +0530

    ShellScripting-RoboShop

--- DevOps/ECommerce-Project/ShellScripting-RoboShop/rabbitmq.sh
+++ DevOps/ECommerce-Project/ShellScripting-RoboShop/rabbitmq.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+source common.sh
 
-yum install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> $log
+func_exit_status
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+yum install rabbitmq-server -y &>> $log
+func_exit_status
+
+echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+systemctl enable rabbitmq-server &>> $log
+systemctl restart rabbitmq-server &>> $log
+func_exit_status
+
+echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+rabbitmqctl add_user roboshop roboshop123 &>> $log
+func_exit_status
+
+echo -e "\n\e[33mResetting mysql root password.\e[0m" | tee -a $log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $log
+func_exit_status
+
+
+
