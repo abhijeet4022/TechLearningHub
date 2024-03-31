b1c1902f96ae5f50b3a2071d017f6ed09c8bf738
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-03-31 14:37:32 +0530

    ShellScripting-RoboShop

--- DevOps/ECommerce-Project/ShellScripting-RoboShop/config.sh
+++ DevOps/ECommerce-Project/ShellScripting-RoboShop/config.sh
@@ -6,27 +6,27 @@ if [ -z "${component}" ]; then
   exit 1
 fi
 
-echo "Install git."
+echo -e "\e[33mInstalling git\e[0m."
 yum install git bash-completion -y &>> $log
 func_exit_status
 
+echo -e "\e[33mCloning the repo.\e[0m."
 if [ ! -d /home/centos/TechLearningHub ]; then
-  echo "Cloning the repo."
   git clone https://github.com/abhijeet4022/TechLearningHub.git &>> $log
   func_exit_status
 else
   echo -e "\e[32mDirectory exist\e[0m"
 fi
 
-
-echo "Changing the directory."
+echo -e "\e[33mChanging the directory.\e[0m."
 cd TechLearningHub/DevOps/ECommerce-Project/ShellScripting-RoboShop/ &>> $log
 func_exit_status
 
-echo "Pulling the code."
+echo -e "\e[33mPulling the code.\e[0m."
 git pull &>> $log
 func_exit_status
 
+echo -e "\e[33mRunning the main script.\e[0m."
 echo "Running the script."
 sudo bash ${component}.sh
 
