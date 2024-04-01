7b65706026a0262b7349d4b9f9b8715358ee76a8
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-01 06:11:48 +0530

    ShellScripting-RoboShop

--- Shell-Scripting/Raghu/learn-shell/03-variable.sh
+++ Shell-Scripting/Raghu/learn-shell/03-variable.sh
@@ -16,3 +16,22 @@ echo ADD of 2+2 = $ADD
 
 
 
+# Access environment variable
+
+env # Is the command to list all environment variable.
+
+echo $variablename
+
+# To declare the environment variables now it will consider for script.
+variable-name=value
+export variablename=value
+
+# Now try to access the variable again.
+echo $variablename
+
+
+
+
+
+
+
