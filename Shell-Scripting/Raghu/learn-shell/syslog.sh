5f00c50ee434fcb48e3243c81be827ea70ab5127
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-03 15:04:34 +0530

    Ansible-RoboShop

--- Shell-Scripting/Raghu/learn-shell/syslog.sh
+++ Shell-Scripting/Raghu/learn-shell/syslog.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cd /var/log/rsyslog
+if [ $? -ne 0 ]; then
+  echo "Directory not exist"
+  exit 1
+fi
+
 for file in messages-*.gz; do
     echo "Processing $file..."
     zgrep -i 'FRHNVNFW01' "$file"
