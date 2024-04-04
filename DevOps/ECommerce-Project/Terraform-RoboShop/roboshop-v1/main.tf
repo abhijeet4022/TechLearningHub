f1e914efdf820700ff560b958abaa14f4b909f2e
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-04 20:18:41 +0530

    Terraform-RoboShop

--- DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v1/main.tf
+++ DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v1/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-#  region = "us-east-1"
+  #  region = "us-east-1"
 }
 
 resource "aws_instance" "frontend" {
-  ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t2.micro"
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["sg-062c9c57661d1416a"]
-  tags = {
-    Name = frontend
+  tags                   = {
+    Name    = frontend
     Project = roboshop
   }
 }
@@ -16,6 +16,6 @@ resource "aws_route53_record" "frontend" {
   zone_id = "Z09678453PONOT92KJ2ZM"
   name    = "frontend-dev.learntechnology.cloud"
   type    = "A"
-  ttl = 10
+  ttl     = 10
   records = [aws_instance.frontend.private_ip]
 }
