07a4671a410be309ed55c0cd94e17fbf2d277619
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-06 18:43:28 +0530

    Terraform-RoboShop

--- DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v3/var.tf
+++ DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v3/var.tf
@@ -13,47 +13,47 @@ variable "project" {
 variable "components" {
   default = {
     frontend = {
-      name          = frontend
+      name          = "frontend"
       instance_type = "t2.micro"
     }
     mongodb = {
-      name          = mongodb
+      name          = "mongodb"
       instance_type = "t2.micro"
     }
     catalogue = {
-      name          = catalogue
+      name          = "catalogue"
       instance_type = "t2.micro"
     }
     redis = {
-      name          = redis
+      name          = "redis"
       instance_type = "t2.micro"
     }
     user = {
-      name          = user
+      name          = "user"
       instance_type = "t2.micro"
     }
     cart = {
-      name          = cart
+      name          = "cart"
       instance_type = "t2.micro"
     }
     mysql = {
-      name          = mysql
+      name          = "mysql"
       instance_type = "t3.small"
     }
     shipping = {
-      name          = shipping
+      name          = "shipping"
       instance_type = "t2.micro"
     }
     rabbitmq = {
-      name          = rabbitmq
+      name          = "rabbitmq"
       instance_type = "t2.micro"
     }
     payment = {
-      name          = payment
+      name          = "payment"
       instance_type = "t2.micro"
     }
     dispatch = {
-      name          = dispatch
+      name          = "dispatch"
       instance_type = "t2.micro"
     }
   }
