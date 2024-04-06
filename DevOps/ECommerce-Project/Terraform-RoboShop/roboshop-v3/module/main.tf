ff482d58c5d6dfe8ada325d488a3bfc35d266a88
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-06 18:41:13 +0530

    Terraform-RoboShop

--- DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v3/module/main.tf
+++ DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v3/module/main.tf
@@ -16,5 +16,18 @@ resource "aws_route53_record" "records" {
   records = [aws_instance.instance.private_ip]
 }
 
-resource "null_resource" "ansible" {}
+resource "null_resource" "ansible" {
+  depends_on = [
+  aws_route53_record.records
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/TechLearningHub/DevOps/ECommerce-Project/Ansible-RoboShop
+git pull
+sleep 60
+ansible-playbook -i ${var.name}-dev.learntechnology.cloud, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
+}
 
--- DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v3/var.tf
+++ DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v3/var.tf
@@ -1,9 +1,9 @@
 variable "zone_id" {
-  default = ""
+  default = "Z09678453PONOT92KJ2ZM"
 }
 
 variable "security_group" {
-  default = ""
+  default = ["sg-062c9c57661d1416a"]
 }
 
 variable "project" {
@@ -13,7 +13,47 @@ variable "project" {
 variable "components" {
   default = {
     frontend = {
-      name = frontend
+      name          = frontend
+      instance_type = "t2.micro"
+    }
+    mongodb = {
+      name          = mongodb
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name          = catalogue
+      instance_type = "t2.micro"
+    }
+    redis = {
+      name          = redis
+      instance_type = "t2.micro"
+    }
+    user = {
+      name          = user
+      instance_type = "t2.micro"
+    }
+    cart = {
+      name          = cart
+      instance_type = "t2.micro"
+    }
+    mysql = {
+      name          = mysql
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name          = shipping
+      instance_type = "t2.micro"
+    }
+    rabbitmq = {
+      name          = rabbitmq
+      instance_type = "t2.micro"
+    }
+    payment = {
+      name          = payment
+      instance_type = "t2.micro"
+    }
+    dispatch = {
+      name          = dispatch
       instance_type = "t2.micro"
     }
   }
