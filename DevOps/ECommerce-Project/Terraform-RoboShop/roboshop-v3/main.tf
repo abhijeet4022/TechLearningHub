8ebaa85dc06d9c1cd780c6581cdeb943fb2b3c4e
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-04-06 18:32:11 +0530

    Terraform-RoboShop

--- DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v3/main.tf
+++ DevOps/ECommerce-Project/Terraform-RoboShop/roboshop-v3/main.tf
@@ -4,6 +4,7 @@ module "components" {
   source         = "./module"
   zone_id        = var.zone_id
   security_group = var.security_group
+  project        = var.project
   name           = each.value["name"]
   instance_type  = each.value["instance_type"]
 }
