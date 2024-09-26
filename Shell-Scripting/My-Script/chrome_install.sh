4a4e53ea53b62afc1c417e64b5d3bd47823d8e1d
abhijeet4022 <abhijeet4022@gmail.com>
2024-09-26 07:35:20 +0530

    Google Chrome Installation Script

--- Shell-Scripting/My-Script/chrome_install.sh
+++ Shell-Scripting/My-Script/chrome_install.sh
@@ -2,28 +2,25 @@
 
 # Download Google Chrome RPM package
 echo "Downloading Google Chrome RPM package..."
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+wget -q -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 
 # Install Google Chrome
 echo "Installing Google Chrome..."
-sudo dnf install -y ./google-chrome-stable_current_x86_64.rpm
+sudo dnf install -y /tmp/google-chrome-stable_current_x86_64.rpm
 
 # Clean up RPM file
-rm -f google-chrome-stable_current_x86_64.rpm
+rm -f /tmp/google-chrome-stable_current_x86_64.rpm
 
 # Add alias to .bashrc
 echo "Setting up alias for Google Chrome..."
-echo "alias chrome='google-chrome --no-sandbox > /dev/null 2>&1 & disown'" >> ~/.bashrc
+#echo "alias chrome='google-chrome --no-sandbox > /dev/null 2>&1 & disown'" >> ~/.bashrc
 
-######################################
-# Define the alias
-#alias_cmd="alias chrome='google-chrome --no-sandbox > /dev/null 2>&1 & disown'"
+#  Check if the alias is already in .bashrc and add it only if not present
+alias_cmd="alias chrome='google-chrome --no-sandbox > /dev/null 2>&1 & disown'"
 
-# Check if the alias is already in .bashrc and add it only if not present
-#if ! grep -qF "$alias_cmd" ~/.bashrc; then
-#    echo "$alias_cmd" >> ~/.bashrc
-#fi
-###########################################
+if ! grep -qF "$alias_cmd" ~/.bashrc; then
+    echo "$alias_cmd" >> ~/.bashrc
+fi
 
 # Apply the changes
 source ~/.bashrc
