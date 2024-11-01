b53a158fbc1a2623bc30f40147d6699551c9ab89
abhijeet4022 <abhijeet4022@gmail.com>
2024-11-01 09:11:28 +0530

    Project App

---
+++ Monitoring/Prometheus/Install/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+  echo "You should run as root user"
+  exit 1
+fi
+
+if [ -d /opt/prometheus ]; then
+  rm -rf /opt/prometheus
+fi
+
+URL=$(curl -L -s https://prometheus.io/download/  | grep tar | grep prometheus- | grep linux-amd64  | sed -e "s|>| |g" -e 's|<| |g' -e 's|"| |g' |xargs -n1 | grep ^http | tail -1)
+
+FILENAME=$(echo $URL | awk -F / '{print $NF}')
+DIRNAME=$(echo $FILENAME | sed -e 's/.tar.gz//')
+
+cd /opt
+curl -s -L -O $URL
+tar -xf $FILENAME
+rm -f $FILENAME
+mv $DIRNAME prometheus
+
+cp /tmp/labautomation/tools/prometheus-server/prometheus.service /etc/systemd/system/prometheus.service
+systemctl enable prometheus
+systemctl start prometheus
---
+++ Monitoring/Prometheus/Readme.md
@@ -0,0 +1,9 @@
+# Prometheus is a metrics monitoring tool.
+# Server Configuration.
+- Install prometheus on prometheus server from `https://prometheus.io/download/`
+
+# Client Configuration.
+- Install Node exporter on Node from - `https://prometheus.io/download/#node_exporter'
+
+* In prometheus server mention the node details on configuration file.
+* 
