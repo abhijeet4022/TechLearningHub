9698551964580e2a7ef6c768ed48b746c3ff6985
abhijeet4022 <abhijeet4022@gmail.com>
2024-11-05 06:06:21 +0530

    Project App

--- Monitoring/Graffana/install.sh
+++ Monitoring/Graffana/install.sh
@@ -3,7 +3,7 @@ git pull &> /tmp/grafana.log
 
 echo -e "\e[1;32mConfiguring Repo\e[0m"
 cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
-'[grafana]
+[grafana]
 name=grafana
 baseurl=https://packages.grafana.com/oss/rpm
 repo_gpgcheck=1
@@ -11,7 +11,7 @@ enabled=1
 gpgcheck=1
 gpgkey=https://packages.grafana.com/gpg.key
 sslverify=1
-sslcacert=/etc/pki/tls/certs/ca-bundle.crt'
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
 EOF
 
 
