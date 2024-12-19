76f4b741f1d16e7faa4d938f026f2daff8efdb1a
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-12-19 18:23:33 +0530

    Modification

--- Shell-Scripting/My-Script/FindLocalAccount.sh
+++ Shell-Scripting/My-Script/FindLocalAccount.sh
@@ -10,6 +10,7 @@ echo "Username,UID,GID,Shell,Hostname" > "$output_file"
 
 # Find local accounts with UID >= 1000 and shell set to /bin/bash, and save to CSV file
 awk -F: -v host="$hostname" '$3 >= 1000 && $7 == "/bin/bash" {print $1 "," $3 "," $4 "," $7 "," host}' /etc/passwd >> "$output_file"
+# awk -F: '$3 >= 1000 && $7 == "/bin/bash" {print $1 "," $3 "," $4 "," $7}' /etc/passwd
 
 # Notify the user about the output file location
 echo "Results saved to $output_file"
