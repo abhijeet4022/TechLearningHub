d3f8d2d7585799bcd23f875b0711d4be60eb3f4b
abhijeet4022 <abhijeet4022@gmail.com>
2024-12-31 11:54:17 +0530

    Kubernetes Cluster Creation

--- Kubernetes/KOPS_Installation/s3.tf
+++ Kubernetes/KOPS_Installation/s3.tf
@@ -1,16 +1,16 @@
-# # S3 Bucket
-# resource "aws_s3_bucket" "bucket" {
-#   bucket           = "learntechnology.cloud" # Replace with a globally unique name
-#   tags             = { Name = "ExampleBucket" }
-#   object_ownership = "BucketOwnerEnforced"
-#
-# }
-#
-# # Optional: Enable Versioning
-# resource "aws_s3_bucket_versioning" "example_bucket_versioning" {
-#   bucket = aws_s3_bucket.bucket.id
-#
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+# S3 Bucket
+resource "aws_s3_bucket" "bucket" {
+  bucket           = "learntechnology.cloud" # Replace with a globally unique name
+  tags             = { Name = "ExampleBucket" }
+  object_ownership = "BucketOwnerEnforced"
+
+}
+
+# Optional: Enable Versioning
+resource "aws_s3_bucket_versioning" "example_bucket_versioning" {
+  bucket = aws_s3_bucket.bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
