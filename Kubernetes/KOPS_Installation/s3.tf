0a27370b02f32a5db764e87ddce46451670bf981
abhijeet4022 <abhijeet4022@gmail.com>
2024-12-31 13:15:28 +0530

    Kubernetes Cluster Creation

--- Kubernetes/KOPS_Installation/s3.tf
+++ Kubernetes/KOPS_Installation/s3.tf
@@ -2,6 +2,7 @@
 resource "aws_s3_bucket" "bucket" {
   bucket           = "cluster.learntechnology.cloud" # Replace with a globally unique name
   tags             = { Name = "ExampleBucket" }
+  force_destroy    = true  # Automatically delete objects when destroying the bucket
 }
 
 # Optional: Enable Versioning
