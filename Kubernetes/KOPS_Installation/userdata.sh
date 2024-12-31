b8462309870a6d81922c7e078763b7618c5b39e0
abhijeet4022 <abhijeet4022@gmail.com>
2024-12-31 13:02:25 +0530

    Kubernetes Cluster Creation

--- Kubernetes/KOPS_Installation/userdata.sh
+++ Kubernetes/KOPS_Installation/userdata.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-apt-get update -y &>> /tmp/userdata.log
-cd /usr/local/bin &>> /tmp/userdata.log
-curl -LO https://github.com/kubernetes/kops/releases/download/v1.30.2/kops-linux-amd64 &>> /tmp/userdata.log
-mv kops-linux-amd64 kops &>> /tmp/userdata.log
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &>> /tmp/userdata.log
-chmod 777 kops &>> /tmp/userdata.log
-chmod  777 kubectl &>> /tmp/userdata.log
 
-cat <<EOF | tee -a ~/.bashrc
+# Update system
+sudo apt-get update -y &>> /tmp/userdata.log
+
+# Change directory
+sudo cd /usr/local/bin &>> /tmp/userdata.log
+
+# Download kops binary
+sudo curl -LO https://github.com/kubernetes/kops/releases/download/v1.30.2/kops-linux-amd64 &>> /tmp/userdata.log
+
+# Rename kops binary
+sudo mv kops-linux-amd64 kops &>> /tmp/userdata.log
+
+# Download kubectl binary
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &>> /tmp/userdata.log
+
+# Set permissions for kops and kubectl
+sudo chmod 777 kops &>> /tmp/userdata.log
+sudo chmod 777 kubectl &>> /tmp/userdata.log
+
+# Add environment variables to .bashrc
+sudo tee -a ~/.bashrc <<EOF
 export NAME=learntechnology.cloud
 export KOPS_STATE_STORE=s3://cluster.learntechnology.cloud
 export AWS_REGION=us-east-1
@@ -17,11 +30,16 @@ alias k=kubectl
 EOF
 
 
-source ~/.bashrc &>> /tmp/userdata.log
+# Source .bashrc
+sudo source ~/.bashrc &>> /tmp/userdata.log
 
-ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N "" &>> /tmp/userdata.log
+# Generate SSH keys
+sudo sudo ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N "" &>> /tmp/userdata.log
 
-mkdir /cluster_deployment &>> /tmp/userdata.log
+# Create a directory for cluster deployment
+sudo mkdir /cluster_deployment &>> /tmp/userdata.log
+
+# Generate cluster.yaml file for cluster deployment
 #kops create cluster --name=learntechnology.cloud \
 #--state=s3://cluster.learntechnology.cloud --zones=us-east-1a,us-east-1b \
 #--node-count=2 --control-plane-count=1 --node-size=t3.medium --control-plane-size=t3.medium \
@@ -29,11 +47,16 @@ mkdir /cluster_deployment &>> /tmp/userdata.log
 #--ssh-public-key ~/.ssh/id_rsa.pub \
 #--dns-zone=learntechnology.cloud --dry-run --output yaml > /cluster_deployment/cluster.yaml
 
-cd /cluster_deployment &>> /tmp/userdata.log
-cp cluster.yaml /cluster_deployment/cluster.yaml &>> /tmp/userdata.log
-kops create -f cluster.yaml &>> /tmp/userdata.log
-kops update cluster --name learntechnology.cloud --yes --admin &>> /tmp/userdata.log
+# Copy cluster.yaml and create the cluster
+sudo cd /cluster_deployment &>> /tmp/userdata.log
+sudo cp cluster.yaml /cluster_deployment/cluster.yaml &>> /tmp/userdata.log
+
+# Create cluster
+sudo kops create -f cluster.yaml &>> /tmp/userdata.log
+
+# Update the cluster
+sudo kops update cluster --name learntechnology.cloud --yes --admin &>> /tmp/userdata.log
+
 # kops validate cluster --wait 10m
 # kops validate cluster --name learntechnology.cloud
 # kops delete cluster --name learntechnology.cloud --yes
-
