b8ba178d0d89d25acbbfdd1ab96deb914fe0edfa
abhijeet4022 <abhijeet4022@gmail.com>
2024-12-31 11:13:39 +0530

    Kubernetes Cluster Creation

--- Kubernetes/KOPS_Installation/management_node.tf
+++ Kubernetes/KOPS_Installation/management_node.tf
@@ -23,5 +23,5 @@ resource "aws_instance" "management_node" {
 
 
 output "management_node_public_ip" {
-  value = "management_node public_ip is ${aws_instance.management_node.public_ip}"
+  value = "management_node public_ip is - ${aws_instance.management_node.public_ip}"
 }
