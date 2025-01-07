9525d20494e5fe977d498d67283c191c7fb5ff03
abhijeet4022 <abhijeet4022@gmail.com>
2025-01-07 07:57:58 +0530

    Kubernetes Cluster - Load Balancer

--- Kubernetes/KOPS_Installation/userdata.sh
+++ Kubernetes/KOPS_Installation/userdata.sh
@@ -9,27 +9,27 @@ EDITOR="/usr/bin/vim"
 DEPLOYMENT_DIR="/cluster_deployment"
 
 # Update system
-echo -e "\e[32mUpdating system packages...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mUpdating system packages...\e[0m" | tee -a ${LOG_FILE}
 sudo apt-get update -y &>> ${LOG_FILE}
 
 # Download and install kops
-echo -e "\e[32mInstalling kops...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mInstalling kops...\e[0m" | tee -a ${LOG_FILE}
 curl -Lo /usr/local/bin/kops https://github.com/kubernetes/kops/releases/download/v1.30.2/kops-linux-amd64 &>> ${LOG_FILE}
 chmod +x /usr/local/bin/kops &>> ${LOG_FILE}
 
 # Download and install kubectl
-echo -e "\e[32mInstalling kubectl...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mInstalling kubectl...\e[0m" | tee -a ${LOG_FILE}
 curl -Lo /usr/local/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &>> ${LOG_FILE}
 chmod +x /usr/local/bin/kubectl &>> ${LOG_FILE}
 
 # Download and install kubens
-echo -e "\e[32mInstalling kubens...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mInstalling kubens...\e[0m" | tee -a ${LOG_FILE}
 sudo curl -Lo /usr/local/bin/kubens.tar.gz https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubens_v0.9.5_linux_x86_64.tar.gz &>> ${LOG_FILE}
 sudo tar -zxf /usr/local/bin/kubens.tar.gz -C /usr/local/bin/ &>> ${LOG_FILE}
 sudo rm -rf LICENSE kubens.tar.gz &>> ${LOG_FILE}
 
 # Add environment variables and alias to /etc/profile
-echo -e "\e[32mConfiguring environment variables and alias...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mConfiguring environment variables and alias...\e[0m" | tee -a ${LOG_FILE}
 if ! grep -q "export NAME=$CLUSTER_NAME" /etc/profile; then
   cat <<EOF >> /etc/profile
 export NAME=$CLUSTER_NAME
@@ -44,17 +44,17 @@ source /etc/profile
 fi
 
 # Generate SSH keys if they do not exist
-echo -e "\e[32mGenerating SSH keys...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mGenerating SSH keys...\e[0m" | tee -a ${LOG_FILE}
 if [ ! -f ~/.ssh/id_rsa ]; then
   ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N "" &>> ${LOG_FILE}
 fi
 
 # Create a directory for cluster deployment
-echo -e "\e[32mCreating cluster deployment directory at $DEPLOYMENT_DIR...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mCreating cluster deployment directory at $DEPLOYMENT_DIR...\e[0m" | tee -a ${LOG_FILE}
 sudo mkdir -p ${DEPLOYMENT_DIR} &>> ${LOG_FILE}
 
 # Generate cluster.yaml for deployment
-echo -e "\e[32mGenerating cluster.yaml configuration...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mGenerating cluster.yaml configuration...\e[0m" | tee -a ${LOG_FILE}
 sudo kops create cluster \
   --name=$CLUSTER_NAME \
   --state=$KOPS_STATE_STORE \
@@ -71,34 +71,34 @@ sudo kops create cluster \
   --dry-run --output yaml > ${DEPLOYMENT_DIR}/cluster.yaml
 
 # Create cluster from the YAML file
-echo -e "\e[32mCreating the cluster...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mCreating the cluster...\e[0m" | tee -a ${LOG_FILE}
 sudo -E kops create -f ${DEPLOYMENT_DIR}/cluster.yaml &>> ${LOG_FILE}
 
 # Update the cluster
-echo -e "\e[32mUpdating the cluster...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mUpdating the cluster...\e[0m" | tee -a ${LOG_FILE}
 sudo -E kops update cluster --name=$CLUSTER_NAME --yes --admin &>> ${LOG_FILE}
-echo -e "\e[32mPlease wait for 10 Min to up the cluster...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mPlease wait for 10 Min to up the cluster...\e[0m" | tee -a ${LOG_FILE}
 sleep 300
 
 # Create the .kube directory for the root user
-echo -e "\e[32mCreate the .kube directory for the root user...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mCreate the .kube directory for the root user...\e[0m" | tee -a ${LOG_FILE}
 sudo mkdir -p /root/.kube  /home/ubuntu/.kube
 
 # Move the Kubernetes configuration file to the root user's .kube directory
-echo -e "\e[32mMove the Kubernetes configuration file to the root and ubuntu user's .kube directory\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mMove the Kubernetes configuration file to the root and ubuntu user's .kube directory\e[0m" | tee -a ${LOG_FILE}
 sudo cp /.kube/config /home/ubuntu/.kube/config
 sudo cp /.kube/config /root/.kube/config
-echo -e "\e[32mSetting the .kube directory ownership and permission...\n\e[0m" | tee -a ${LOG_FILE}
+echo -e "\n\e[32mSetting the .kube directory ownership and permission...\e[0m" | tee -a ${LOG_FILE}
 sudo chown -R root:ubuntu /home/ubuntu/.kube
 sudo chmod -R 770 /root/.kube /home/ubuntu/.kube
 
 sleep 300
 # Output a completion message
-echo -e "\e[32mKubernetes configuration for root user has been set up successfully.\n\e[0m" | tee -a "$LOG_FILE"
+echo -e "\n\e[32mKubernetes configuration for root user has been set up successfully.\e[0m" | tee -a "$LOG_FILE"
 
 
 ## Optional: Validate the cluster
-#echo -e "\e[32mValidating the cluster (optional)...\n\e[0m" | tee -a ${LOG_FILE}
+#echo -e "\n\e[32mValidating the cluster (optional)...\e[0m" | tee -a ${LOG_FILE}
 #kops validate cluster --name=$CLUSTER_NAME --wait 10m &>> ${LOG_FILE}
 # kops validate cluster --name learntechnology.cloud --wait 10m
 # kops delete cluster --name learntechnology.cloud --yes
