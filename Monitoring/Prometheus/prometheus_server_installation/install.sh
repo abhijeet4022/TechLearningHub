1baeae0e891a4b9ddbfd4a6d85847d878195e8f6
abhijeet4022 <abhijeet4022@gmail.com>
2026-01-11 15:02:45 +0530

    Linux Topics

--- Monitoring/Prometheus/prometheus_server_installation/install.sh
+++ Monitoring/Prometheus/prometheus_server_installation/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+echo -e "\e[1;32mUpdating the script\e[0m"
 git pull
 if [ $(id -u) -ne 0 ]; then
   echo "You should run as root user"
   exit 1
 fi
 
+echo -e "\e[1;32mRemoving old Prometheus installation if any\e[0m"
 if [ -d /opt/prometheus ]; then
   systemctl stop prometheus
   rm -rf /opt/prometheus /etc/systemd/system/prometheus.service
@@ -15,7 +18,7 @@ fi
 #FILENAME=$(echo $URL | awk -F / '{print $NF}')
 #DIRNAME=$(echo $FILENAME | sed -e 's/.tar.gz//')
 
-echo "Copying Prometheus service file"
+echo -e "\e[1;32mCopying Prometheus service file\e[0m"
 cp ./prometheus.service /etc/systemd/system/prometheus.service
 
 #cd /opt
@@ -25,11 +28,19 @@ cp ./prometheus.service /etc/systemd/system/prometheus.service
 #rm -f $FILENAME
 #mv $DIRNAME prometheus
 
+echo -e "\e[1;32mChange Prometheus directories\e[0m"
 cd /opt
+
 echo -e "\e[1;32mDownload Prometheus package\e[0m"
 curl -L -s -O https://github.com/prometheus/prometheus/releases/download/v3.9.1/prometheus-3.9.1.linux-amd64.tar.gz
+
+echo -e "\e[1;32mExtracting Prometheus package\e[0m"
 tar -xf  prometheus-3.9.1.linux-amd64.tar.gz
+
+echo -e "\e[1;32mCleaning up\e[0m"
 rm -rf prometheus-3.9.1.linux-amd64.tar.gz
+
+echo -e "\e[1;32mRenaming Prometheus directory\e[0m"
 mv prometheus-3.9.1.linux-amd64 prometheus
 
 echo -e "\e[1;32mStarting Prometheus service\e[0m"
